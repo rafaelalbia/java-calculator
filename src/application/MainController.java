5da1ee02ee4ac78bb29c05e3b9b077e8f7d0e5ff
rafaelalbia <rafaelalbia@outlook.com>
2020-07-15 01:42:13 -0300

    Initial import

---
+++ src/application/MainController.java
@@ -0,0 +1,13 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainController {
+	@FXML
+	private Label result;
+	public void peformCalculation(ActionEvent event) {
+		
+	}
+}
