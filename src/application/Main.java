b8f452c701ae207371cc389cf41f1ba7a331dc1e
rafaelalbia <rafaelalbia@outlook.com>
2020-07-29 22:16:12 -0300

    Annotations

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,6 @@
 package application;
 
+// Imports
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,19 +9,33 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
+    
+    // Stage and scene parameters
     public void start(Stage primaryStage) {
+    	
+    	// The program will try to execute, the exception type will be printed if diagnosed
     	try {
+    		
+    		// Here the base of application is defined, the size and style of the window
     		Parent root = FXMLLoader.load(getClass().getResource("/application/Main.fxml"));
     		Scene scene = new Scene(root, 450, 352);
     		scene.getStylesheets().add(getClass().getResource("Application.css").toExternalForm());
+    		
+    		// The stage will receive the scene and will be shown
     		primaryStage.setScene(scene);
     		primaryStage.show();
+    		
+    		// Title
+    		primaryStage.setTitle("Java Calculator");
     	} catch (Exception e) {
     		e.printStackTrace();
     	}
     }
     
+    // Main part of application
     public static void main(String[] args) {
+    	
+    	// The application is launched
     	launch(args);
     }
 }
