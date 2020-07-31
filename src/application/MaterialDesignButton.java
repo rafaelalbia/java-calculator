b94272b461a013acc608230997e6d62d7dc59f65
rafaelalbia <rafaelalbia@outlook.com>
2020-07-30 21:54:08 -0300

    Update ripple effect

--- src/application/MaterialDesignButton.java
+++ src/application/MaterialDesignButton.java
@@ -1,13 +1,18 @@
 package application;
 
-// Imports
+import com.sun.javafx.scene.control.skin.ButtonSkin;
+import javafx.animation.FadeTransition;
+import javafx.animation.Interpolator;
+import javafx.animation.SequentialTransition;
+import javafx.animation.Timeline;
 import javafx.scene.control.Button;
+import javafx.scene.control.Skin;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 
-// Ripple effect
+// Ripple effect structure
 public class MaterialDesignButton extends Button {
 	
 	// Ripple effect variables
@@ -23,8 +28,44 @@ public class MaterialDesignButton extends Button {
 		
 		getStyleClass().addAll("md-button");
 		
+		// Will call the function with the effect
 		createRippleEffect();
 	}
 	
+	@Override
+	protected Skin<?> createDefaultSkin() {
+		final ButtonSkin buttonSkin = new ButtonSkin(this);
+		// Adding circleRipple as first node of button nodes to be on the bottom
+		this.getChildren().add(0, circleRipple);
+		return buttonSkin;
+	}
 	
+	private void createRippleEffect() {
+		circleRipple = new Circle(0.1, rippleColor);
+		circleRipple.setOpacity(0.0);
+		// Optional box blur on ripple - smoother ripple effect
+//		circleRipple.setEffect(new BoxBlur(3, 3, 2));
+		
+		
+		// Fade effect bit longer to show edges on the end
+		final FadeTransition fadeTransition = new FadeTransition(rippleDuration, circleRipple);
+		fadeTransition.setInterpolator(Interpolator.EASE_OUT);
+		fadeTransition.setFromValue(1.0);
+		fadeTransition.setToValue(0.0);
+		
+		final Timeline scaleRippleTimeline = new Timeline();
+		
+		final SequentialTransition parallelTransition = new SequentialTransition();
+		parallelTransition.getChildren().addAll(
+				scaleRippleTimeline,
+				fadeTransition
+			);
+		
+		parallelTransition.setOnFinished(event1 -> {
+			circleRipple.setOpacity(0.0);
+			circleRipple.setRadius(0.1);
+		});
+		
+		
+	}
 }
