2d478c33b02754cd268f0bee11a97d6907c9ceff
rafaelabia <rafaelalbia@outlook.com>
2020-09-29 23:36:44 -0300

    Interface import

--- src/application/MainFrame.java
+++ src/application/MainFrame.java
@@ -3,8 +3,11 @@ package application;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
 
 public class MainFrame {
 
@@ -13,6 +16,21 @@ public class MainFrame {
 
     @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
+
+    @FXML
+    private AnchorPane window;
+
+    @FXML
+    private Label screen;
+
+    @FXML
+    private Pane keys;
+
+    @FXML
+    private Button zero, one, two, three, four, five, six, seven, eight, nine, dot;
+
+    @FXML
+    private Button percent, division, multiplication, subtraction, addition, equal, backstep, clear;
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
